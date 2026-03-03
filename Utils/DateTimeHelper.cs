3dc175465cdc32867cc815780ceb8bebcc97f7e1
EricDuong5423 <tuduong05042003@gmail.com>
2026-03-04 01:16:47 +0700

    fix: desync time in sever azure

--- Utils/DateTimeHelper.cs
+++ Utils/DateTimeHelper.cs
@@ -1,13 +1,15 @@
-﻿namespace CO4029_BE.Utils;
-
-public static class DateTimeHelper
+﻿public static class DateTimeHelper
 {
     public static DateTime GetVnNow()
     {
-        var timeZoneId = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows)
-            ? "SE Asia Standard Time"
-            : "Asia/Ho_Chi_Minh";
+        DateTime utcNow = DateTime.UtcNow;
+        
+        TimeZoneInfo vnTimeZone = TimeZoneInfo.FindSystemTimeZoneById(
+            System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows) 
+                ? "SE Asia Standard Time" 
+                : "Asia/Ho_Chi_Minh"
+        );
 
-        return TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById(timeZoneId));
+        return TimeZoneInfo.ConvertTimeFromUtc(utcNow, vnTimeZone);
     }
 }
